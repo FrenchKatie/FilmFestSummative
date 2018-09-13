c4f72929a56053796dd572ac52dfa38104cac6e5
izsi-salmon <izsi.salmon@gmail.com>
2018-09-13 19:26:11 +1200

    managed to access json and inject it into dom

--- server/script.js
+++ server/script.js
@@ -1,14 +1,44 @@
-console.log("JS is loaded into the page");
+var data;
+// ----------DOM QUERIES----------
+
+var anchor = document.getElementById('anchor');
+
+// Film data containers
+
+var filmImg = document.getElementsByClassName('film-image');
+var filmTitle = document.getElementsByClassName('film-title');
+var filmDirector = document.getElementsByClassName('film-director');
+
+// -------------------------------
 
 $.ajax({
-    url: 'http://192.168.33.10:3000/',
     type: 'GET',
+    url: '/data/film-data.json',
     dataType: 'json',
-    success:function(data){
-        console.log(data);
+    success:function(jsonData){
+        data = jsonData;
+        console.log(jsonData);
+        createContainers();
     },
     error:function(error){
         console.log('ERROR');
         console.log(error);
     }
-})
+});
+
+function createContainers(){
+    for (var i = 0; i < data.length; i++) {
+        var filmContainer = document.createElement('div');
+        filmContainer.setAttribute('class','film-ctnr');
+        filmContainer.innerHTML = '<img src="" class="film-image" style="width: 200px"><span class="film-title"></span><span class="film-director"></span>';
+        anchor.after(filmContainer);
+        initData(i);
+    }
+}
+
+function initData(i){
+    filmImg[0].setAttribute('src', data[i].images[0]);
+    filmTitle[0].innerHTML = data[i].title;
+    filmDirector[0].innerHTML = data[i].director[0];
+    
+}
